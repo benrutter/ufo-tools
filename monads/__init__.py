2f746d10262756898964c83e60be52b844768bc1
Ben <ben@FLEX-PC2909>
2024-01-09 17:06:07 +0000

    more types

--- monads/__init__.py
+++ monads/__init__.py
@@ -1,48 +1,55 @@
-from typing import Any
+from typing import Any, Type, TypeVar, Callable, Optional, Union
+
+T = TypeVar("T")
+U = TypeVar("U")
 
 class Monad:
-    def __init__(self: Any, value):
-        self.value = value
+    def __init__(self: Any, value: T) -> None:
+        self.value: T = value
 
-    def bind(self, func):
+    def bind(self, func) -> "Monad":
         return Monad(func(self.value))
 
-    def pipe(self, funcs):
+    def pipe(self, *funcs) -> "Monad":
         result = self
         for func in funcs:
             result = self.bind(func)
         return result
 
-    def __or__(self, other):
+    def __or__(self, other: Callable) -> "Monad":
         return self.bind(other)
 
-    def __rshift__(self, other):
+    def __rshift__(self, other) -> "Monad":
         return self.bind(other)
 
-    def unwrap(self):
+    def unwrap(self) -> T:
         return self.value
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.__class__.__name__}({self.value})"
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.__str__()
 
 class Maybe(Monad):
-    def bind(self, func):
+    def bind(self, func: Callable) -> "Maybe":
         if self.value is None:
             return Maybe(None)
         return Maybe(func(self.value))
 
 class List(Monad):
-    def bind(self, func):
+
+    def __init__(self: Any, value: list) -> None:
+        self.value: list = value
+
+    def bind(self, func: Callable) -> "List":
         return List([func(x) for x in self.value])
 
-    def filter(self, func):
+    def filter(self, func: Callable) -> "List":
         return List([i for i in self.value if func(i)])
 
 class Result(Monad):
-    def __init__(self, value, exception = None, traceback = None):
+    def __init__(self, value: T, exception: Optional[Type[Exception]] = None):
         self.value = value
         self.exception = exception
 
@@ -52,12 +59,17 @@ class Result(Monad):
         try:
             return Result(func(self.value))
         except Exception as exception:
-            return Result(self.value, exception)
+            return Result(None, exception)
 
-    def unwrap(self):
+    def unwrap(self) -> T:
         if self.exception:
             raise self.exception
         return self.value
 
-    def __str__(self):
+    def unwrap_or(self, value: U) -> Union[T, U]:
+        if self.exception:
+            return value
+        return self.value
+
+    def __str__(self) -> str:
         return f"{self.__class__.__name__}({self.exception or self.value})"
